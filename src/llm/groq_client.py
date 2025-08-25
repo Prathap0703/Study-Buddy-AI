d22dfe88681f6eb51da2845cfff9e3d3179b8f3c
Prathap0703 <vprathap2004@gmail.com>
2025-08-25 20:28:41 +0530

    changes made

--- src/llm/groq_client.py
+++ src/llm/groq_client.py
@@ -1,9 +1,11 @@
 from langchain_groq import ChatGroq
 from src.config.settings import settings
 
+import streamlit as st
+
 def get_groq_llm():
     return ChatGroq(
-        api_key = settings.GROQ_API_KEY,
-        model = settings.MODEL_NAME,
-        temperature=settings.TEMPERATURE
+        api_key = st.secrets["GROQ_API_KEY"],
+        model = st.secrets["MODEL_NAME"],
+        temperature = float(st.secrets["TEMPERATURE"])
     )
